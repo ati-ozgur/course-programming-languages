f8a7104dfcb0c9c3bf5066e579fde61a9c849e94
Atilla Özgür <ati.ozgur@gmail.com>
2026-01-19 22:12:30 +0300

    float_for_loop java added

---
+++ course-content/examples/float_for_loop1.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args) {
+	
+		float f = 10.0f;
+	    System.out.println(f);
+		for (int i=0;i<10 ;i++ ) {
+		    f = f / 10;
+		    System.out.println(f);
+		}
+	    System.out.println("");		
+		for (int i=0;i<10 ;i++ ) {
+		    f = f * 10;			
+		    System.out.println(f);
+		}
+
+    }
+}
+
