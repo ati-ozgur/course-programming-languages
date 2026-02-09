02e7363defc67820b9c65d77d001c77ab8f8c927
Atilla Ozgur <ati.ozgur@gmail.com>
2026-02-09 20:28:12 +0100

    conditional statements v1

---
+++ course-content/examples/switch_example1.java
@@ -0,0 +1,32 @@
+public class switch_example1 {
+    public static void main(String[] args) {
+	
+	int day = 4;
+	switch (day) {
+	  case 1:
+	    System.out.println("Pazartesi");
+	    break;
+	  case 2:
+	    System.out.println("Salı");
+	    break;
+	  case 3:
+	    System.out.println("Çarşamba");
+	    break;
+	  case 4:
+	    System.out.println("Perşembe");
+	    break;
+	  case 5:
+	    System.out.println("Cuma");
+	    break;
+	  case 6:
+	    System.out.println("Cumartesi");
+	    break;
+	  case 7:
+	    System.out.println("Pazar");
+	    break;
+}
+
+
+    }
+}
+
---
+++ course-content/examples/switch_example2.java
@@ -0,0 +1,25 @@
+public class switch_example1 {
+    public static void main(String[] args) {
+	
+	int day = 4;
+	switch (day) {
+	  case 1:
+	    System.out.println("Pazartesi");
+	  case 2:
+	    System.out.println("Salı");
+	  case 3:
+	    System.out.println("Çarşamba");
+	  case 4:
+	    System.out.println("Perşembe");
+	  case 5:
+	    System.out.println("Cuma");
+	  case 6:
+	    System.out.println("Cumartesi");
+	  case 7:
+	    System.out.println("Pazar");
+}
+
+
+    }
+}
+
